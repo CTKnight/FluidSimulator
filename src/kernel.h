091d0674f4379da5990777d564bd480ff02028a3
Lai Jiewen <alanljw12345@gmail.com>
2020-05-02 23:09:49 -0700

    Reuse kernel in CUDA

--- src/kernel.h
+++ src/kernel.h
@@ -1,10 +1,11 @@
 
 #include "real.h"
 #include "vector3R.h"
+#include "cuda/cudaUtils.h"
 
 #define PI (3.14159265358979323)
 
-inline REAL W_poly6(const Vector3R &r, REAL h) {
+CUDA_CALLABLE inline REAL W_poly6(const Vector3R &r, REAL h) {
   const auto r2 = r.norm2();
   const auto h2 = pow(h, 2);
   if (r2 > h2) {
@@ -14,7 +15,7 @@ inline REAL W_poly6(const Vector3R &r, REAL h) {
 }
 
 // https://www.wolframalpha.com/input/?i=gradient+15%2F%28PI*h%5E6%29*%28h-x%29%5E3
-inline REAL W_spiky_gradient(const Vector3R &r_vec, REAL h) {
+CUDA_CALLABLE inline REAL W_spiky_gradient(const Vector3R &r_vec, REAL h) {
   const auto r = r_vec.norm();
   if (r > h) {
     return 0;
